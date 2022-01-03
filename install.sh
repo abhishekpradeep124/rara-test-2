4eddd0dd0ce7d5bf6333a919ed95cc067403f8b5
abhishekpradeep124 <89505135+abhishekpradeep124@users.noreply.github.com>
2022-01-03 15:52:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,10 @@ curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021
 openssl sha1 -sha256 kubectl
 chmod +x ./kubectl
 sudo cp ./kubectl /usr/local/bin/kubectl 
-kubectl version --short --client
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-eksctl version
+
 
 # install Helm
 # https://helm.sh/docs/intro/install/#from-script
@@ -23,3 +22,9 @@ mv linux-amd64/helm /usr/local/bin/helm
 
 # install S3 plugin for Helm
 helm plugin install https://github.com/hypnoglow/helm-s3.git --version $HELM_S3_PLUGIN_VERSION
+
+aws --version
+kubectl version --short --client
+eksctl version
+helm version
+

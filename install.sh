d47941dd4cd696e3f896fd2888d31be44ee41ede
abhishekpradeep124 <89505135+abhishekpradeep124@users.noreply.github.com>
2022-01-03 15:33:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,22 @@
-#!/bin/sh
+#! /usr/bin/env bash
 
 set -e
 
-apk add --update py3-pip curl openssl wget
+sudo apt install awscli -y
+
 
 HELM_S3_PLUGIN_VERSION="0.10.0"
 AWS_DEFAULT_REGION="ap-southeast-1"
-
 # install kubectl
 curl -L "https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
 kubectl version --client
+
+sudo cp ./kubectl /usr/local/bin/kubectl 
+kubectl version --short --client
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
 
 # install Helm
 # https://helm.sh/docs/intro/install/#from-script
@@ -24,3 +30,5 @@ helm plugin install https://github.com/hypnoglow/helm-s3.git --version $HELM_S3_
 # cleanup
 rm /var/cache/apk/*
 rm -rf /tmp/*
+
+
